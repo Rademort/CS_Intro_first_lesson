07fe0e75227b3d377b5c55c8e571f704023e848a
Roman <r.i.tihun@gmail.com>
2023-01-29 16:42:49 +0300

    Вторая и нулевая задачи

---
+++ 000/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вводятся два числа. Найти и вывести на экран их суммую
+
+int a, b, sum;
+System.Console.Write("Введите a: " );
+a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b: " );
+b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Сумма a и b равна: " );
+sum = a + b;
+System.Console.WriteLine(sum);
+System.Console.WriteLine(a + "+" + b.ToString() + "=" + Convert.ToString(sum) );
+System.Console.WriteLine("{0:F2}+{1}={2:G4}",a,b,sum);
+System.Console.WriteLine($"{a:F2}+{b}={sum:G4}");
--- 001/Program.cs
+++ 001/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// 1.С клавиатуры вводится целое число. Вывести квадрат числа 
+
+int a;
+System.String s;
+System.Console.Write("Введите число: ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+System.Console.WriteLine("Квадрат числа равен: " + (a * a));

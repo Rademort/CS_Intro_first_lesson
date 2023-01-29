6242deb1d8aa232a067da7f3098078eea8ac724d
Roman <r.i.tihun@gmail.com>
2023-01-29 17:38:53 +0300

    Lesson 01_1

---
+++ 003/Program.cs
@@ -0,0 +1,18 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+int a, b, max;
+
+System.Console.WriteLine("Введите число a: ");
+a = b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+System.Console.WriteLine($"max={max}");
---
+++ 007/Program.cs
@@ -0,0 +1,9 @@
+﻿// Выяснить является ли число чётным.
+
+int N = 8;
+bool odd = N%2 == 0;
+if(odd == true){
+    System.Console.WriteLine($"Число {N} четное");
+} else{
+    System.Console.WriteLine($"Число {N} нечетное");
+}

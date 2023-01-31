771e054aabe8fdd0ab343005b4087dc0fa1fef7a
Roman <r.i.tihun@gmail.com>
2023-02-01 02:30:47 +0300

    Задача 5_1

---
+++ 006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+double a, y;
+System.Console.Write("Введите число: ");
+a = Convert.ToDouble(Console.ReadLine());
+y = Math.Sin(a);
+System.Console.WriteLine("Синус числа "+a+" равен: " + y);
+
+// тут не уточняется, синус угла или радиана.

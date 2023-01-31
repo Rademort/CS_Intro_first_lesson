9a8e3ce414f36d233a8d988e3914f9ac1c854bd7
Roman <r.i.tihun@gmail.com>
2023-02-01 02:50:35 +0300

    Задача 8

--- 008/Program.cs
+++ 008/Program.cs
@@ -1,1 +1,9 @@
 ﻿// Вывести на экран числа от -N до N
+System.Console.Write("Введите целое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i;
+i = -N;
+while(i<=N){
+    System.Console.Write(i + " ");
+    i++;
+}

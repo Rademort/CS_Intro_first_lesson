0243beafbdf42a624dda185de1c05be49f2030d3
Roman <r.i.tihun@gmail.com>
2023-02-01 03:18:04 +0300

    Задача 10

--- 010/Program.cs
+++ 010/Program.cs
@@ -1,1 +1,5 @@
 ﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+System.Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int lastNumber = N%10;
+System.Console.WriteLine("Последняя цифра введенного числа: " + lastNumber);
---
+++ 012/Program.cs
@@ -0,0 +1,19 @@
+﻿// С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+System.Console.Write("Введите число от 10 до 99: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >= 10 & N <= 99)
+{
+    int d0 = N % 10;
+    int d1 = N / 10 % 10;
+    int max = d0;
+    if (d1 > max)
+    {
+        max = d1;
+        System.Console.WriteLine("Наибольшая цифра числа: " + max);
+    }
+    else { System.Console.WriteLine("Наибольшая цифра числа: " + max); }
+}
+else
+{
+    System.Console.WriteLine("Введенное число не соответствует диапазону от 10 до 99");
+}

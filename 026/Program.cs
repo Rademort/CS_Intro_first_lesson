aee277c9a39cb3b1d0d491c8ffa8e527be138910
Roman <r.i.tihun@gmail.com>
2023-02-04 18:51:26 +0300

    04.02.23

---
+++ 026/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+bool isPal(int n)
+{
+    int r = 0, nn = n;
+    while (n > 0)
+    {
+        r = r * 10 + n % 10;
+        n = n / 10;
+    }
+    return r == nn;
+}
+
+int n;
+Console.Write("n=");
+n = Convert.ToInt32(Console.ReadLine());
+if (isPal(n))
+    Console.WriteLine("Палиндром");
+else
+    Console.WriteLine("Не палиндром");
+
---
+++ 034/Program.cs
@@ -0,0 +1,19 @@
+﻿//34. Определить количество цифр в числе. Сделать подпрограмму.
+
+int CounterDigits(int n)
+{
+    if (n == 0)
+        return 1;//пограничный случай
+
+    int counter = 0;//счетчик
+    while (n != 0)
+    {
+        counter++;
+        n /= 10;//n=n/10
+    }
+    return counter;
+}
+
+//Вывод только в основной подпрограмме
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(CounterDigits(N));

8502fa2a1aab87654860ba822888e11d49cd5540
Roman <r.i.tihun@gmail.com>
2023-02-14 19:44:37 +0300

    14.02.23 ДЗ к 5 уроку

---
+++ 054/Program.cs
@@ -0,0 +1,25 @@
+﻿// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+System.Console.Write("Введите число N для нахождения первых N чисел Фибоначчи: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+FibonacciArray(array);
+PrintArray(array);
+
+void FibonacciArray(int[] a)
+{
+    a[0] = 0;
+    a[1] = 1;
+    for (int i = 2; i < a.Length; i++)
+    {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+}
+
+void PrintArray(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        System.Console.Write($"{a[i]} ");
+    }
+}
---
+++ 056/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу копирования массива
+int[] arr1 = { 1, 2, 3, 4 };
+int[] arr2 = new int[arr1.Length];
+
+CoppyArray(arr1, ref arr2);
+PrintArray(arr2);
+
+void CoppyArray(int[] a1, ref int[] a2) // вроде как есть уже готовые методы для любых типов массивов. Но я так только придумал
+{
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        a2[i] = a1[i];
+    }
+}
+
+void PrintArray(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        System.Console.Write($"{a[i]} ");
+    }
+}
